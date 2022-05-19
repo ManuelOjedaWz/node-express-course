94e9cef7b7708772448c97fdbc4443edc530c4c2
Manuel Ojeda <manuel.ojeda@wizeline.com>
2022-05-18 20:19:49 -0600

    [FEAT]: Added EJS

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-/* eslint-disable n/no-path-concat */
 const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname + '/static')))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'))
+
+app.use(express.static(path.join(__dirname, './static')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/static/index.html'))
+  res.render('pages/index', {
+    pageTitle: 'Welcome'
+  })
 })
 
 app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname + '/static/speakers.html'))
+  res.sendFile(path.join(__dirname, './static/speakers.html'))
 })
 
 app.listen(port, () => {
