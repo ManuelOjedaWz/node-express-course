2538a1798e1a0a7ae4cf54a9c08fb581f48dab1f
Manuel Ojeda <manuel.ojeda@wizeline.com>
2022-05-19 10:16:44 -0600

    Cookies added

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const path = require('path')
 const routes = require('./routes')
+const cookieSession = require('cookie-session')
 const app = express()
 const port = 3000
+
+app.set('trust proxy', 1)
 
 const FeedbackService = require('./services/FeedbackService')
 const SpeakerService = require('./services/SpeakerService')
 
 const feedbackService = new FeedbackService('./data/feedback.json')
 const speakerService = new SpeakerService('./data/speakers.json')
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [
+    'kjkjkhfskdskhdsfkhdnsk',
+    'nhuifdhnufsdnhdfjkvdsh'
+  ]
+}))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'))
